2c365a1fc60715973ad9bf605953b7548ec870a6
Asim <mohdasim.srk@gmailcom>
2023-07-03 12:00:52 +0530

    cors

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const cors=require("cors");
 const server=jsonServer.create();
 const router=jsonServer.router("db.json");
 
+server.use(cors());
 server.use(router);
-server.use(cors())
 
 server.listen(8080,()=>{
     console.log("server is running at 8080")
